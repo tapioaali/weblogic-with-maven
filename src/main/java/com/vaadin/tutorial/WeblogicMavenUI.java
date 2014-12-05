9218f3428a0840ed2a776b82aed8fd97fa4b3967
Tapio Aali <tapio@vaadin.com>
2014-12-05 22:46:25 +0200

    Formatted UI code

--- src/main/java/com/vaadin/tutorial/WeblogicMavenUI.java
+++ src/main/java/com/vaadin/tutorial/WeblogicMavenUI.java
@@ -6,16 +6,14 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
 
 @Theme("valo")
 @SuppressWarnings("serial")
-public class WeblogicMavenUI extends UI
-{
+public class WeblogicMavenUI extends UI {
 
     @WebServlet(value = "/*", asyncSupported = true)
     @VaadinServletConfiguration(productionMode = false, ui = WeblogicMavenUI.class)
@@ -27,9 +25,15 @@ public class WeblogicMavenUI extends UI
         final VerticalLayout layout = new VerticalLayout();
         layout.setMargin(true);
         setContent(layout);
-        
-        Label label = new Label("Weblogic running successfully!");
-        layout.addComponent(label);
+
+        Label titleLabel = new Label(
+                "Vaadin Weblogic Deployment Test Application");
+        titleLabel.addStyleName(ValoTheme.LABEL_H1);
+        layout.addComponent(titleLabel);
+
+        Label otherLabel = new Label("Application running successfully!");
+        otherLabel.addStyleName(ValoTheme.LABEL_SUCCESS);
+        layout.addComponent(otherLabel);
     }
 
 }
